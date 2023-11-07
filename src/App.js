e49e9495df5f2c54b1f1f9b2828454630a7d869a
hyewon-han <hw1635@hanmail.net>
2023-11-07 11:18:52 +0900

    삭제기능 구현완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const initialState = [
+    {
+      id: 1,
+      title: "hello",
+      contents: "title contents",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "hello!!",
+      contents: "title contents",
+      isDone: false,
+    },
+  ];
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const submitTodo = (e) => {
+    e.preventDefault();
+    const todoObj = { id: todos.length + 1, title, contents, isDone: false };
+    setTodos([...todos, todoObj]);
+    setTitle("");
+    setContents("");
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>ToDoList</header>
+      <main>
+        <form>
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            type="text"
+            placeholder="Write your todo title"
+            required
+          ></input>
+          <input
+            value={contents}
+            onChange={(e) => {
+              setContents(e.target.value);
+            }}
+            type="text"
+            placeholder="Write your todo content"
+            required
+          ></input>
+          <button onClick={submitTodo}>추가</button>
+        </form>
+        <ul>
+          {todos.map((todo, idx) => {
+            return (
+              <li key={idx} className="todo">
+                <h2>{todo.title}</h2>
+                <p>{todo.contents}</p>
+                <button onClick={() => deleteTodo(todo.id)}>삭제</button>
+                <button>완료</button>
+              </li>
+            );
+          })}
+        </ul>
+        <ul></ul>
+      </main>
     </div>
   );
 }
