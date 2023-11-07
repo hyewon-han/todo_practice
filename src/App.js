1706a686ff3f45cd33afdeecbd2c68fc5b5b982b
hyewon-han <hw1635@hanmail.net>
2023-11-07 11:39:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   const initialState = [
     {
       id: 1,
-      title: "hello",
+      title: "test1",
       contents: "title contents",
       isDone: false,
     },
     {
       id: 2,
-      title: "hello!!",
+      title: "test2!!",
       contents: "title contents",
       isDone: false,
     },
@@ -28,6 +28,14 @@ function App() {
   };
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const isDoneTodo = (id) => {
+    const todoObj = (todos.find((todo) => todo.id === id).isDone = true);
+    setTodos([...todos, todoObj]);
+  };
+  const isNotDoneTodo = (id) => {
+    const todoObj = (todos.find((todo) => todo.id === id).isDone = false);
+    setTodos([...todos, todoObj]);
   };
   return (
     <div>
@@ -54,20 +62,38 @@ function App() {
           ></input>
           <button onClick={submitTodo}>추가</button>
         </form>
+        <h1>Working..!🔥</h1>
         <ul>
-          {todos.map((todo, idx) => {
-            return (
-              <li key={idx} className="todo">
-                <h2>{todo.title}</h2>
-                <p>{todo.contents}</p>
-                <button onClick={() => deleteTodo(todo.id)}>삭제</button>
-                <button>완료</button>
-              </li>
-            );
-          })}
+          {todos
+            .filter((todo) => todo.isDone === false)
+            .map((todo, idx) => {
+              return (
+                <li key={idx} className="todo">
+                  <h2>{todo.title}</h2>
+                  <p>{todo.contents}</p>
+                  <button onClick={() => deleteTodo(todo.id)}>삭제</button>
+                  <button onClick={() => isDoneTodo(todo.id)}>완료</button>
+                </li>
+              );
+            })}
         </ul>
-        <ul></ul>
+        <h1>Done..!🎉</h1>
+        <ul>
+          {todos
+            .filter((todo) => todo.isDone === true)
+            .map((todo, idx) => {
+              return (
+                <li key={idx} className="todo">
+                  <h2>{todo.title}</h2>
+                  <p>{todo.contents}</p>
+                  <button onClick={() => deleteTodo(todo.id)}>삭제</button>
+                  <button onClick={() => isNotDoneTodo(todo.id)}>취소</button>
+                </li>
+              );
+            })}
+        </ul>
       </main>
+      <footer> Hyewon's To Do List &copy; All right reserved</footer>
     </div>
   );
 }
